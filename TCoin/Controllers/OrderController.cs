7661d0a104c70d7d9bdaa70db15a94692ac6e243
Ihor Andrushchenko <IAndrushchenko@lan.ad>
2018-07-09 14:02:47 +0300

    Add project files.

---
+++ TCoin/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TCoin.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return new JsonResult(new {name="igor",age=23 });
+            //return View();
+        }
+        
+    }
+}
